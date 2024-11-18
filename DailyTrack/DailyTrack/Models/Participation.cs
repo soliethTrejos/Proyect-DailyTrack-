ffcf43142bfcd907db14a73ff95ad280be2e4daf
jeyni02 <jeynilomary@uamv.edu.ni>
2024-11-18 16:41:51 -0600

    Models

---
+++ DailyTrack/DailyTrack/Models/Participation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyTrack.Models
+{
+    internal class Participation
+    {
+        public int Id { get; set; }
+        public int EventId { get; set; }
+        public DateTime RegisteredOn { get; set; }
+    }
+}
